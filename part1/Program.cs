65a69dd4dc39a7519b2ad4c50142460430cc1d55
Petri <petri.kivipuro@gmail.com>
2020-05-06 15:49:17 +0300

    Creating working environmetn

---
+++ part1/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace part1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HEllo vorlr");
+        }
+    }
+}
---
+++ part1/exercise_1.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace part1
+{
+    public class Numbers
+    {
+        public int Sum(int x)
+        {
+            return 0;
+        }
+    }
+}
