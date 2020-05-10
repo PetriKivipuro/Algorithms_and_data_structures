7591a564d0eb12690bd07e19ad29bcbfe85f7777
Petri <petri.kivipuro@gmail.com>
2020-05-10 11:38:26 +0300

    ex2 and prog preparation

--- part1/Program.cs
+++ part1/Program.cs
@@ -7,11 +7,20 @@ namespace part1
     {
         static void Main(string[] args)
         {
+            Substrings subs = new Substrings();
+            Console.WriteLine(subs.Calculate("aybabtu", "bab")); // 1
+            Console.WriteLine(subs.Calculate("aaaaa", "aa")); // 4
+            Console.WriteLine(subs.Calculate("monkey", "banana")); // 0
+
+            // Alla exercise 1
+
             Numbers num = new Numbers();
             Console.WriteLine(num.Sum(4075)); // 16
             Console.WriteLine(num.Sum(3)); // 3
             Console.WriteLine(num.Sum(999999999)); // 81
-            Console.WriteLine(num.Sum(1111111111)); // Integerejä ei voi olla enempää kuin 10
+            Console.WriteLine(num.Sum(1111111111)); // Integerejä ei voi olla enempää kuin
+
+
         }
     }
 }
---
+++ part1/exercise_2.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace part1
+{
+    public class Substrings
+    {
+        public int Calculate(string a, string b)
+        {
+            return 0;
+
+        }
+    }
+}
