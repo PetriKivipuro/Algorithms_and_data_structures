452af373d113dac02bd1d6d5ee2636769a80c69c
Petri <petri.kivipuro@gmail.com>
2020-05-11 10:01:49 +0300

    c

--- part1/exercise_2.cs
+++ part1/exercise_2.cs
@@ -6,8 +6,30 @@ namespace part1
     {
         public int Calculate(string a, string b)
         {
+
             return 0;
 
         }
     }
 }
+
+/*
+
+string a muutetaan listaksi
+samoin string b
+
+string b:tä verrataan string a:han
+    b listan ensimmäinen arvo käydään läpi verraten a listaan
+
+    jos b ensimmäistä arvoa ei löydy == 0
+
+    jos b listan ensimmäinen löytyy
+    otetaan b listan toinen ja verrataan löytyykö b1 + be
+
+    jos listän ensimmäinen ja toinen löytyy otetan b listan kolmas arvo
+    jnejne
+
+    kun kaikki on käyty läpi verrataan montako b listaa löytyy kokonaisuudessaan a listasta
+
+    ja annetaan tulos
+*/
