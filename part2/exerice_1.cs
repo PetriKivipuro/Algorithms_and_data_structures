a9f9c03ec7f14c14aaee1dc00121aaf8d967e5a9
Petri <petri.kivipuro@gmail.com>
2020-05-17 19:11:39 +0300

    ...notworking

--- part2/exerice_1.cs
+++ part2/exerice_1.cs
@@ -1,41 +1,42 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Text;
 
 namespace part2
 {
     public class Effi
     {
-        public string Something();
+        public int Something(string n);
         {
             DateTime start = DateTime.Now;
         int sum = 0;
-            for (int i = 0; i = 0 -1; i++)
+
+            for (int i = 0; i<= n.Lenght - 1; i++)
             {
 
-                for (int j = 0; j = 0 -1; j++)
+                for (int j = 0; j<= n.Lenght - 1; j++)
                 {
-                    if (char[i]==0 && char[i]==1)
+                    if (n[i] == 0 && n[i] == 1)
                     {
-                        sum +=1;
+                        sum += 1;
                     }
 }
-return sum;
             }
-            DateTime end = DateTime.Now;
+    DateTime end = DateTime.Now;
 Console.WriteLine("time: " end.Substract(start));
-        }
-    }
+                   return Console.WriteLine(sum);
+                }
 }
 
 
-        public string SomethingMore();
+/*         public string SomethingMore();
         {
             DateTime start = DateTime.Now;
-int sum = 0;
-int zeros = 0;
+        int sum = 0;
+        int zeros = 0;
 
-for(int i = 0; i= 0; i++;)
+for(int i = 0; i< 0; i++;)
 {
 if(chars[i] == 0)
 {
@@ -53,3 +54,4 @@ Console.WriteLine("time: " end.Substract(start));
     }
 
 }
+ */
