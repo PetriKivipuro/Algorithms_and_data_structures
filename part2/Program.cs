42908cdba6726e68f58f521ce1caa8bad699046b
Petri <petri.kivipuro@gmail.com>
2020-05-17 21:25:58 +0300

    fixgffoundex1

--- part2/Program.cs
+++ part2/Program.cs
@@ -10,51 +10,22 @@ namespace part2
         {
 
             Effi pl = new Effi();
-            Console.WriteLine(pl.Something("01001"));
+            Console.WriteLine(pl.Something("10"));
+            Console.WriteLine(pl.Something("100"));
+            Console.WriteLine(pl.Something("1000"));
+            Console.WriteLine(pl.Something("10000"));
+
+
+            Console.WriteLine();
+
+            Console.WriteLine(pl.Somethingmore("10"));
+            Console.WriteLine(pl.Somethingmore("100"));
+            Console.WriteLine(pl.Somethingmore("1000"));
+            Console.WriteLine(pl.CreateInput(1));
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-            /*             List<int> numbers = new List<int>();
-                        int sum = 0;
-                        int n = 10;
-                        int x = 8;
-                        for (int i = 0; i > (n - 1); i++)
-                        {
-                            if (numbers[i] == x)
-                            {
-                                sum++;
-                            }
-
-
-                        }
-                        Console.WriteLine(sum + n + ", " + x + " qhr");
-             */
-            /*
-                        // time complexity O(1)
-                        int a = 5;
-                        int b = 3;
-                        int c = a + b;
-
-
-                        if (c > 0)
-                        {
-                            Console.WriteLine(a + b);
-                        }
-             */
         }
     }
 }
--- part2/exerice_1.cs
+++ part2/exerice_1.cs
@@ -17,7 +17,7 @@ namespace part2
 
                 for (int j = 0; j <= n.Length - 1; j++)
                 {
-                    if (n[i].ToString() == "0" && n[i].ToString() == "1")
+                    if (n[i].ToString() == "0" && n[j].ToString() == "1")
                     {
                         sum += 1;
                     }
@@ -26,6 +26,42 @@ namespace part2
             DateTime end = DateTime.Now;
             Console.WriteLine("time: " + end.Subtract(start));
             return (sum);
+        }
+
+        public int Somethingmore(string z)
+        {
+
+            DateTime start1 = DateTime.Now;
+            int sum = 0;
+            int zeros = 0;
+
+            for (int i = 0; i <= z.Length - 1; i++)
+            {
+                if (z[i].ToString() == "0")
+                {
+                    zeros += 1;
+                }
+                else
+                {
+                    sum += zeros;
+                }
+
+            }
+
+            DateTime end1 = DateTime.Now;
+            Console.WriteLine("time: " + end1.Subtract(start1));
+            return sum;
+
+        }
+        public string CreateInput(int n)
+        {
+            StringBuilder sb = new StringBuilder();
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                sb.Append(random.Next(0, 2).ToString());
+            }
+            return sb.ToString();
         }
     }
 }
