83d51342e1690a2ced6b94844b14c14191d762b0
Petri <petri.kivipuro@gmail.com>
2020-05-22 12:42:50 +0300

    cleanup

--- part3/exerice_1.cs
+++ part3/exerice_1.cs
@@ -9,59 +9,9 @@ namespace part3
     {
         public int Something(string n)
         {
-            DateTime start = DateTime.Now;
-            int sum = 0;
-
-            for (int i = 0; i <= n.Length - 1; i++)
-            {
-
-                for (int j = 0; j <= n.Length - 1; j++)
-                {
-                    if (n[i].ToString() == "0" && n[j].ToString() == "1")
-                    {
-                        sum += 1;
-                    }
-                }
-            }
-            DateTime end = DateTime.Now;
-            Console.WriteLine("time: " + end.Subtract(start));
-            return (sum);
+            return 0;
         }
 
-        public int Somethingmore(string z)
-        {
 
-            DateTime start1 = DateTime.Now;
-            int sum = 0;
-            int zeros = 0;
-
-            for (int i = 0; i <= z.Length - 1; i++)
-            {
-                if (z[i].ToString() == "0")
-                {
-                    zeros += 1;
-                }
-                else
-                {
-                    sum += zeros;
-                }
-
-            }
-
-            DateTime end1 = DateTime.Now;
-            Console.WriteLine("time: " + end1.Subtract(start1));
-            return sum;
-
-        }
-        public string CreateInput(int n)
-        {
-            StringBuilder sb = new StringBuilder();
-            Random random = new Random();
-            for (int i = 0; i < n; i++)
-            {
-                sb.Append(random.Next(0, 2).ToString());
-            }
-            return sb.ToString();
-        }
     }
 }
