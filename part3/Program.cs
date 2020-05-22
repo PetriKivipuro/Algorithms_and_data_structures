e1fdf52d42cf8b73396f70bf2f07bdc65c22ab87
Petri <petri.kivipuro@gmail.com>
2020-05-22 13:30:32 +0300

    adjust and test ex1part3

--- part3/Program.cs
+++ part3/Program.cs
@@ -8,8 +8,25 @@ namespace part3
     {
         static void Main(string[] args)
         {
+            // tää on tehty class systeemillä
             Recu moi = new Recu();
             moi.Hello(5);
+
+            Console.WriteLine();
+
+            // Sama homma tehty mainissa
+            Hello1(5);
+
+            static void Hello1(int n)
+            {
+
+                for (int i = 0; i <= n - 1; i++)
+                {
+                    Console.WriteLine("Hello!");
+                }
+
+
+            }
         }
     }
 }
