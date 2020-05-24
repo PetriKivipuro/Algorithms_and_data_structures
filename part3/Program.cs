181af0d82296e7533c876591497986199c5a9ddf
Petri <petri.kivipuro@gmail.com>
2020-05-24 19:16:30 +0300

    ex2 set

--- part3/Program.cs
+++ part3/Program.cs
@@ -8,25 +8,35 @@ namespace part3
     {
         static void Main(string[] args)
         {
-            // tää on tehty class systeemillä
-            Recu moi = new Recu();
-            moi.Hello(5);
 
-            Console.WriteLine();
+            Sorting s = new Sorting();
+            int[] sortMe = Randomizer(100);
+            int[] sortMeLarge = Randomizer(1000000);
+            s.QuickSort(sortMe);
+            s.MergeSort(sortMe);
+            s.QuickSort(sortMeLarge);
+            s.MergeSort(sortMeLarge);
 
-            // Sama homma tehty mainissa
-            Hello1(5);
+            // Harjoitus 1
+            /*   // tää on tehty class systeemillä
+              Recu moi = new Recu();
+              moi.Hello(5);
 
-            static void Hello1(int n)
-            {
+              Console.WriteLine();
 
-                for (int i = 0; i <= n - 1; i++)
-                {
-                    Console.WriteLine("Hello!");
-                }
+              // Sama homma tehty mainissa
+              Hello1(5);
 
+              static void Hello1(int n)
+              {
 
-            }
+                  for (int i = 0; i <= n - 1; i++)
+                  {
+                      Console.WriteLine("Hello!");
+                  }
+   */
+
         }
     }
+}
 }
