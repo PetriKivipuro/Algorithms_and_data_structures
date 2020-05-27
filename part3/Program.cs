6731bd4b016f71afaa897c8b9b4e1a8013d893e5
Petri <petri.kivipuro@gmail.com>
2020-05-27 12:58:13 +0300

    d

--- part3/Program.cs
+++ part3/Program.cs
@@ -8,9 +8,9 @@ namespace Part3
         {
 
             Revolutions r = new Revolutions();
-            Console.WriteLine(m.Calculate(new int[] { 4, 1, 3, 2, 5 })); // 3
-            Console.WriteLine(m.Calculate(new int[] { 1, 2, 3, 4, 5 })); // 1
-            Console.WriteLine(m.Calculate(new int[] { 5, 4, 3, 2, 1 })); // 5
+            Console.WriteLine(r.Calculate(new int[] { 4, 1, 3, 2, 5 })); // 3
+            Console.WriteLine(r.Calculate(new int[] { 1, 2, 3, 4, 5 })); // 1
+            Console.WriteLine(r.Calculate(new int[] { 5, 4, 3, 2, 1 })); // 5
 
 
             /*           Sorting s = new Sorting();
