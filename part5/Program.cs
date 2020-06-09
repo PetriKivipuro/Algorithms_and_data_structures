d6d425e22f3218cac48d0121fee2cf4563b25402
Petri <petri.kivipuro@gmail.com>
2020-06-09 16:46:36 +0300

    create ex3  base

---
+++ part5/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+namespace Part5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Connectivity c = new Connectivity(6);
+            c.AddConnection(1, 2);
+            c.AddConnection(2, 3);
+            c.AddConnection(1, 3);
+            c.AddConnection(3, 4);
+            c.AddConnection(5, 6);
+            Console.WriteLine(c.Calculate(1)); // 3
+
+
+        }
+    }
+}
---
+++ part5/exercise_2.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Part5
+{
+    public class Connectivity
+    {
+        public int Connectivity(int n)
+        {
+            return 0;
+        }
+
+        public void AddConnection(int a, int b)
+        {
+            return 0;
+        }
+
+        public int Calculate(int x)
+        {
+
+        }
+    }
+}
