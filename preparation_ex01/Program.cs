fa70b0af3fd57af721a0328122d329cb4c7314c9
Petri <petri.kivipuro@gmail.com>
2020-10-03 20:03:01 +0300

    changes

---
+++ preparation_ex01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace preparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // exercise 1
+            Numbers num = new Numbers();
+            Console.WriteLine(num.Steps(11)); // 5
+            Console.WriteLine(num.Steps(15)); // 5
+            Console.WriteLine(num.Steps(2)); // 2
+            Console.WriteLine(num.Steps(20));
+
+
+        }
+    }
+}
---
+++ preparation_ex01/exercise_01.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace preparation
+{
+    // exercise 1
+    class Numbers
+    {
+        public int Steps(int n)
+        {
+            int sum = 0;
+            int steps = 0;
+            int i = 1;
+            while (true)
+            {
+                steps = steps + 1;
+                sum = sum + i;
+                i++;
+                if (sum >= n)
+                {
+                    break;
+                }
+            }
+            return steps;
+        }
+    }
+
+
+
+
+}
