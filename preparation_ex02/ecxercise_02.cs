22bb8bd9f19fafd937a29d683225d72f8c924b7b
Petri <petri.kivipuro@gmail.com>
2020-10-03 20:11:26 +0300

    separate preparations exercises to clarify life

---
+++ preparation_ex02/ecxercise_02.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace preparation_ex02
+{
+    class ShortenArray
+    {
+        int Calculate(int[] input)
+        {
+
+        }
+
+
+
+
+    }
+}
